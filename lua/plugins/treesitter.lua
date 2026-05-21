13cbd5b47c5abddfb0656f7d28af11c5a9a44a3c
johnSjo <john.sjostrom79@gmail.com>
2026-05-21 10:46:27 +0200

    updating tree-sitter to newer version and simplyfy the setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,14 @@
-return { -- Highlight, edit, and navigate code
+return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-        ensure_installed = {
+    config = function()
+        require('nvim-treesitter').setup()
+
+        require('nvim-treesitter').install {
+            'rust',
+            'javascript',
+            'zig',
             'bash',
             'c',
             'diff',
@@ -17,31 +21,17 @@ return { -- Highlight, edit, and navigate code
             'vim',
             'vimdoc',
             'typescript',
-            'javascript',
-        },
-        -- Autoinstall languages that are not installed
-        auto_install = true,
-        highlight = {
-            enable = true,
-            -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-            --  If you are experiencing weird indenting issues, add the language to
-            --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-            additional_vim_regex_highlighting = { 'ruby' },
-        },
-        indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-        -- Run the standard setup
-        require('nvim-treesitter.configs').setup(opts)
+            'tsx',
+        }
 
         -- Handle MDX manually without an extra plugin
         vim.filetype.add { extension = { mdx = 'mdx' } }
         vim.treesitter.language.register('markdown', 'mdx')
+
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
     end,
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
